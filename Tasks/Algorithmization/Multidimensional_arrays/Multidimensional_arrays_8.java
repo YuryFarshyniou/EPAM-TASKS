36f6e0f99c3969873937cb15780cf403b1db831c
yurachel <yurachel5@rambler.ru>
2021-01-16 17:04:37 +0300

    h

--- Tasks/Algorithmization/Multidimensional_arrays/Multidimensional_arrays_8.java
+++ Tasks/Algorithmization/Multidimensional_arrays/Multidimensional_arrays_8.java
@@ -8,7 +8,9 @@ public class Multidimensional_arrays_8 {
     public static void main(String[] args) {
         int [][] array = {{1,2,3,4},{1,2},{7,8,9,12,1,12}};
 
-
+        for (int i = 0; i < array.length; i++) {
+            
+        }
 
     }
 }
