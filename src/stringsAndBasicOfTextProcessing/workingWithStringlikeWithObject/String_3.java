f294236026b9bfd6ed5c6f28805921e099d5cb78
yurachel <yurachel5@rambler.ru>
2021-02-02 18:14:29 +0300

    Added tasks from 1 to 4 String like with Object

---
+++ src/stringsAndBasicOfTextProcessing/workingWithStringlikeWithObject/String_3.java
@@ -0,0 +1,32 @@
+package stringsAndBasicOfTextProcessing.workingWithStringlikeWithObject;
+
+// Проверить,является ли заданное слово палиндромом.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class String_3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter any string: ");
+        String palindrome = reader.readLine();
+
+        // Избавляемся от всех пробелов и различных знаков.
+
+        String regex = "[\\s+|\\W+]";
+        String strAfterReplaces= palindrome.replaceAll(regex, "");
+
+        StringBuilder stringBuilder = new StringBuilder(strAfterReplaces);
+
+        // Переворачиваем строку для сравнения символов.
+
+        stringBuilder.reverse();
+
+
+        // Сравниваем символы.
+
+        boolean isPalindrome = strAfterReplaces.equalsIgnoreCase(stringBuilder.toString());
+        System.out.println((isPalindrome) ? "The string is palindrome" : "The string is not palindrome");
+    }
+}
